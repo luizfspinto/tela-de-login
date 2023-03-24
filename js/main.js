6340b4a3bda5acd642a424ae394f6c0cf46f823b
luizfspinto <luizfspintoo@gmail.com>
2023-03-24 11:17:22 -0300

    styles form

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function togglePassowrd() {
+  window.document.querySelectorAll(".eye").forEach((eye) => {
+    eye.classList.toggle("hide")
+  })
+
+  if (senha.getAttribute("type") == "password") {
+    senha.setAttribute("type", "text")
+  } else {
+    senha.setAttribute("type", "password")
+  }
+}
